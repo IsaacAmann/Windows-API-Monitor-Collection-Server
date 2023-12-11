19e40a268ec0a54097bb848fa15a3e8fdb81b2f5
IsaacAmann <isaacmnn@gmail.com>
2023-12-11 11:40:45 -0600

    Generated Spring Boot application

---
+++ CollectionServer/src/main/java/com/CollectionServer/CollectionServerApplication.java
@@ -0,0 +1,13 @@
+package com.CollectionServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionServerApplication.class, args);
+	}
+
+}
---
+++ CollectionServer/src/test/java/com/CollectionServer/CollectionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CollectionServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
