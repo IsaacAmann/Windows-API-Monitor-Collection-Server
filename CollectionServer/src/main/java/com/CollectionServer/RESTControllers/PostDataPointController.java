a11c5a02140a39aa8ef7afecc76cff938baee660
IsaacAmann <isaacmnn@gmail.com>
2024-02-18 15:38:08 -0500

    Created REST controller for post datapoint api call

---
+++ CollectionServer/src/main/java/com/CollectionServer/RESTControllers/PostDataPointController.java
@@ -0,0 +1,19 @@
+package com.CollectionServer.RESTControllers;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class PostDataPointController
+{
+	
+}
