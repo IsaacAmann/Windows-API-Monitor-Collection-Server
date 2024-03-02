95eda8ae432f933db3c628e9109da13f5ec77302
IsaacAmann <74680621+IsaacAmann@users.noreply.github.com>
2024-03-02 13:25:28 -0600

    React setup

--- Frontend/windows-api-monitor-dashboard/src/App.js
+++ Frontend/windows-api-monitor-dashboard/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import HomePage from "./Pages/HomePage/HomePage.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<LoginInfoContext.Provider >
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<HomePage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </LoginInfoContext.Provider>
   );
 }
 
---
+++ Frontend/windows-api-monitor-dashboard/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function HomePage()
+{
+	
+	
+	return(
+		<>
+			<p> test </p>
+		</>
+	);
+}
+
+export default HomePage;
