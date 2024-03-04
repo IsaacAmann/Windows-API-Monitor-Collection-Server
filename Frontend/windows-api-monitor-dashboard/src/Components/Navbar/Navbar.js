f8bf44a1881486a139edb3c5de9f117ce6039892
IsaacAmann <isaacmnn@gmail.com>
2024-03-03 23:50:45 -0600

    Setup login. Added drawer to contain logout button and account information

--- Frontend/windows-api-monitor-dashboard/src/Components/Navbar/Navbar.js
+++ Frontend/windows-api-monitor-dashboard/src/Components/Navbar/Navbar.js
@@ -14,6 +14,7 @@ import Button from '@mui/material/Button';
 import { jwtDecode } from "jwt-decode";
 
 import GitHubIcon from '@mui/icons-material/GitHub';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 import {LoginInfoContext} from "../../App.js"
 import Backdrop from '@mui/material/Backdrop';
@@ -22,6 +23,7 @@ import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import Drawer from '@mui/material/Drawer';
 
 import APICallContainer from "../../APICallContainer.js";
 
@@ -32,6 +34,7 @@ function Navbar()
 {
 	const loginInfo = useContext(LoginInfoContext);
 	const [loginUp, setLoginUp] = useState(false);
+	const [profileDisplayUp, setProfileDisplayUp] = useState(false);
 	
 	const [username, setUsername] = useState(null);
 	const [password, setPassword] = useState(null);
@@ -39,6 +42,20 @@ function Navbar()
 	
 	const usernameRef = React.useRef(null);
 	const passwordRef = React.useRef(null);
+	
+	function ProfileDisplayDrawer()
+	{
+		const drawerList = (
+			<Box sx={{width: 250, bgcolor:'#020a07', minHeight: '100%'}} role="presentation" >
+				<p>test</p>
+			</Box>
+		);
+		return(
+			<Drawer anchor="right" open={profileDisplayUp} sx={{}} onClose={() => setProfileDisplayUp(false)}>
+				{drawerList}
+			</Drawer>
+		);
+	}
 	
 	function handleLogin()
 	{
@@ -52,7 +69,6 @@ function Navbar()
 				{
 					loginInfo.setToken(value.token);
 					setLoginUp(false);
-					console.log("L");
 				}
 				else
 				{
@@ -77,7 +93,9 @@ function Navbar()
 		{
 			return(
 				<>
-					<Avatar>N</Avatar>
+					<IconButton onClick={() => setProfileDisplayUp(true)} >
+						<AccountCircleIcon fontSize="large"/>
+					</IconButton>
 				</>
 			);
 		}
@@ -88,7 +106,7 @@ function Navbar()
 		return(
 		<Backdrop open={loginUp} sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
 			<Container maxWidth='md' sx={{bgcolor: 'grey'}}>
-				<IconButton onClick={() => setLoginUp(false)} size="medium" >
+				<IconButton onClick={() => setLoginUp(false)} fontSize="medium" >
 					<CloseIcon/>
 				</IconButton>
 				<Typography variant="h3">Sign In</Typography>
@@ -115,15 +133,16 @@ function Navbar()
 			<Toolbar>
 				<LoginForm/>
 				<Typography component="div" sx={{flexGrow: 1}}>
-					Behavior Based Process Analysis{loginInfo.token}
+					Behavior Based Process Analysis
 				</Typography>
 				<a href="https://github.com/IsaacAmann/Windows-API-Monitor">
 					<GitHubIcon fontSize="large" sx={{mx: 2}}/>
 				</a>
 				<LoginButton/>
-			
 			</Toolbar>
+			<ProfileDisplayDrawer/>
 		</AppBar>
+		
 	</Box>
 	);
 }
