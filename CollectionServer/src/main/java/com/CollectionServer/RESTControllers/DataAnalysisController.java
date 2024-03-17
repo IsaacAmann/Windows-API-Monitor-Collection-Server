8c235a28c58de5220c5e8e9bc0ac9679c302a526
IsaacAmann <isaacmnn@gmail.com>
2024-03-17 01:13:37 -0400

    Created DataAnalysisController class

---
+++ CollectionServer/src/main/java/com/CollectionServer/RESTControllers/DataAnalysisController.java
@@ -0,0 +1,20 @@
+package com.CollectionServer.RESTControllers;
+
+import com.CollectionServer.ClientManagement.CollectionClient;
+import com.CollectionServer.ClientManagement.CollectionClientRepository;
+import com.CollectionServer.DataPointEntity;
+import com.CollectionServer.DataPointRepository;
+import com.CollectionServer.UserManagement.UserAccount;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class DataAnalysisController
+{
+	
+}
