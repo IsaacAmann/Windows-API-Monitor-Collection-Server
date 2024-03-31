eb7b82550898e7529385129897c2cbdca6395478
IsaacAmann <isaacmnn@gmail.com>
2024-03-31 18:42:02 -0500

    Setup logging service

---
+++ CollectionServer/src/main/java/com/CollectionServer/Services/AdminNotificationService.java
@@ -0,0 +1,66 @@
+package com.CollectionServer.Services;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class AdminNotificationService
+{
+    public static final int MAX_ENTRIES = 5000;
+    public enum LogLevel
+    {
+        ERROR,
+        WARN,
+        INFO
+    }
+
+    private static ArrayList<AdminNotificationLog> logsArray = new ArrayList<AdminNotificationLog>();
+
+    public void submitLog(LogLevel logLevel, String loggerName, String message)
+    {
+        AdminNotificationLog log = new AdminNotificationLog(logLevel, loggerName, message);
+        //Check that maximum size is not being exceeded
+        if(logsArray.size() >= MAX_ENTRIES)
+        {
+            //Remove the oldest element
+            logsArray.remove(0);
+        }
+
+        logsArray.add(log);
+    }
+
+    public class AdminNotificationLog
+    {
+        public LogLevel logLevel;
+        public String message;
+
+        public String loggerName;
+
+        public Date dateCreated;
+
+        public AdminNotificationLog(LogLevel logLevel, String loggerName, String message)
+        {
+            this.logLevel = logLevel;
+            this.loggerName = loggerName;
+            this.message = message;
+
+            this.dateCreated = new Date();
+        }
+
+        @Override
+        public String toString()
+        {
+            String output = "";
+
+            output += dateCreated.toString() + " | ";
+            output += logLevel.toString() + " | ";
+            output += loggerName + " >>>> ";
+
+            return output;
+        }
+    }
+}
